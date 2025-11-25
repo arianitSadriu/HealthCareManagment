25066455282620a2f6bceaeb8e32ce53a57dca24
arianitSadriu <122325048+arianitSadriu@users.noreply.github.com>
2025-11-25 01:39:11 +0100

    Base shape of Classes

--- model/Person.java
+++ model/Person.java
@@ -1,4 +1,18 @@
 package HealthCareManagment.model;
 public abstract class  Person{
 
+    protected int id;
+    protected String name;
+    protected String phone;
+    protected String email;
+
+    public Person(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public abstract String getDetails();
+
+
 }
